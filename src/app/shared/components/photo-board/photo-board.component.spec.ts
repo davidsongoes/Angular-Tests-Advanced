e3f4157159ff5d4074b295f837dd28610fcb0e22
Davidson Góes <davidsongoes@gmail.com>
2022-06-06 16:54:07 -0300

    feat(photo-board): :white_check_mark: implementa teste

---
+++ src/app/shared/components/photo-board/photo-board.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PhotoBoardComponent } from './photo-board.component';
+import { PhotoBoardModule } from './photo-board.module';
+
+describe(PhotoBoardComponent.name, () => {
+  let fixture: ComponentFixture<PhotoBoardComponent>;
+  let component: PhotoBoardComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PhotoBoardModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PhotoBoardComponent);
+    component = fixture.componentInstance;
+  });
+
+  it(`Shpuld display rows and columns when (@Input photos) has value`, () => {});
+});
