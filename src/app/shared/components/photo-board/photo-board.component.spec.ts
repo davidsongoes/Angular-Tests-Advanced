3391caa72a706cf97886836d3045dcef92b98477
Davidson Góes <davidsongoes@gmail.com>
2022-06-07 15:12:57 -0300

    test(photo-board): :white_check_mark: implementa teste com array mock

--- src/app/shared/components/photo-board/photo-board.component.spec.ts
+++ src/app/shared/components/photo-board/photo-board.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Photo } from './models/photo.model';
 import { PhotoBoardComponent } from './photo-board.component';
 import { PhotoBoardModule } from './photo-board.module';
+
+function buildPhotosList(): Photo[] {
+  const photos: Photo[] = [];
+  for (let i = 0; i < 8; i++) {
+    photos.push({
+      id: i + 1,
+      url: '',
+      description: '',
+    });
+  }
+  return photos;
+}
 
 describe(PhotoBoardComponent.name, () => {
   let fixture: ComponentFixture<PhotoBoardComponent>;
@@ -15,5 +28,15 @@ describe(PhotoBoardComponent.name, () => {
     component = fixture.componentInstance;
   });
 
-  it(`Shpuld display rows and columns when (@Input photos) has value`, () => {});
+  fit(`Should display rows and columns when (@Input photos) has value`, () => {
+    component.photos = buildPhotosList();
+    fixture.detectChanges();
+    expect(component.rows.length).withContext('Number of Rows').toBe(2);
+    expect(component.rows[0].length)
+      .withContext('Number of columns from the first row')
+      .toBe(4);
+    expect(component.rows[1].length)
+      .withContext('Number of columns from the second row')
+      .toBe(4);
+  });
 });
