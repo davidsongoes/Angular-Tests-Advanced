acc98b4ee9c6bb1601347c3483f5d5c06c142e42
Davidson Góes <davidsongoes@gmail.com>
2022-06-08 16:33:38 -0300

    feat(photo-list): :sparkles: Criando componente

---
+++ src/app/components/photo-list/photo-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Photo } from 'src/app/shared/components/photo-board/models/photo.model';
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['photo-list.component.css'],
+})
+export class PhotoListComponent implements OnInit {
+  public photos$: Observable<Photo[]>;
+  // public likes = 0;
+
+  // public like(): void {
+  //   this.likes++;
+  // }
+  constructor(private service: PhotoBoardService) {}
+
+  public ngOnInit(): void {
+    this.photos$ = this.service.getPhotos();
+  }
+}
