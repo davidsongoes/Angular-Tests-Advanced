3722365e6289671d01069650c5b788577a6737cf
Davidson Góes <davidsongoes@gmail.com>
2022-06-08 16:34:28 -0300

    refactor(app-component): :hammer: Remove seletor e implementa router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
-import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
 import { HttpClientModule } from '@angular/common/http';
-import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
+import { PhotoListModule } from './components/photo-list/photo-list.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LikeWidgetModule,
-    PhotoFrameModule,
-    HttpClientModule,
-    PhotoBoardModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PhotoListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
