07b9196915fea7ba4721bd9ba03838522c78a3c9
Davidson Góes <davidsongoes@gmail.com>
2022-06-08 16:56:43 -0300

    feat(photo-list): :sparkles: implementa spinner no loader

--- src/app/components/photo-list/photo-list.component.ts
+++ src/app/components/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+
 import { Photo } from 'src/app/shared/components/photo-board/models/photo.model';
 import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
 
@@ -10,6 +12,7 @@ import { PhotoBoardService } from 'src/app/shared/components/photo-board/service
 })
 export class PhotoListComponent implements OnInit {
   public photos$: Observable<Photo[]>;
+  public fa = { faCircleNotch };
   // public likes = 0;
 
   // public like(): void {
