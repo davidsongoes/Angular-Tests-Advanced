3087178a40bd3ddef03a45c45d9a255df106145d
Davidson Góes <davidsongoes@gmail.com>
2022-06-09 16:47:01 -0300

    feat(build-photos-list): :sparkles: implementa método isolado em arquivo

---
+++ src/app/shared/components/photo-board/test/build-photos-list.ts
@@ -0,0 +1,13 @@
+import { Photo } from '../models/photo.model';
+
+export function buildPhotosList(): Photo[] {
+  const photos: Photo[] = [];
+  for (let i = 0; i < 8; i++) {
+    photos.push({
+      id: i + 1,
+      url: '',
+      description: '',
+    });
+  }
+  return photos;
+}
