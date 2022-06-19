2f0834a8f115afbb0c6557dffe9973546f9295f6
Davidson Goes <davidsongoes@gmail.com>
2022-06-18 22:48:42 -0300

    feat(photo-board-service): :construction: implementando configuração do arquivo de teste

--- src/app/shared/components/photo-board/services/photo-board.service.spec.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -1,3 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { PhotoBoardService } from './photo-board.service';
 
-describe(PhotoBoardService.name, () => {});
+describe(PhotoBoardService.name, () => {
+  let service: PhotoBoardService;
+  let controller: HttpTestingController;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PhotoBoardService],
+    }).compileComponents();
+    service = TestBed.inject(PhotoBoardService);
+    controller = TestBed.inject(HttpTestingController);
+  });
+});
