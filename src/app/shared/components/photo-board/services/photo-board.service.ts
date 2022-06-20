082ef175c2d9b42d690391fad3f727a4bfd21165
Davidson Goes <davidsongoes@gmail.com>
2022-06-19 22:16:08 -0300

    refactor(photo-board.service): :hammer: refatora retorno para alterar propriedade do método

--- src/app/shared/components/photo-board/services/photo-board.service.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map, tap } from 'rxjs/operators';
 import { Photo } from '../models/photo.model';
 
 @Injectable()
@@ -11,6 +11,14 @@ export class PhotoBoardService {
   public getPhotos(): Observable<Photo[]> {
     return this.http
       .get<Photo[]>(`http://localhost:3000/photos`)
+      .pipe(
+        map((photos) => {
+          return photos.map((photo) => {
+            return { ...photo, description: photo.description.toUpperCase() };
+          });
+        })
+      )
+      .pipe(tap((photos) => console.log(photos)))
       .pipe(delay(3000));
   }
 }
